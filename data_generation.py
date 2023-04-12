86ba3c3df0ef2ea8434f472ac57a2813b0d5ce6c
Leon <king0980692@gmail.com>
2023-04-12 16:30:57 +0800

    update data generation.py

--- data_generation.py
+++ data_generation.py
@@ -10,7 +10,7 @@ def generate_news(raw_data_path):
     for mode in ['train', 'dev', 'test']:
         Path(f'./data/speedy_data/{mode}').mkdir(parents=True, exist_ok=True)
         fsave = open('./data/speedy_data/{}/docs.tsv'.format(mode), 'w', encoding='utf-8')
-        for line in open(os.path.join(raw_data_path, f'MINDlarge_{mode}/news.tsv'), encoding='utf-8'):
+        for line in open(os.path.join(raw_data_path, f'MIND-{args.mode}/train/news.tsv'), encoding='utf-8'):
             doc_id, category, subcategory, title, abstract, url, entities, abstract_entites = line.strip().split('\t')
             body = ' '
             fsave.write(
@@ -19,7 +19,7 @@ def generate_news(raw_data_path):
 
 def generate_traindata(raw_data_path, file_num=512):
     data = []
-    for line in open(os.path.join(raw_data_path, 'MINDlarge_train/behaviors.tsv'), 'r', encoding='utf-8'):
+    for line in open(os.path.join(raw_data_path, f'MIND-{args.mode}/train/behaviors.tsv'), 'r', encoding='utf-8'):
         impid, uid, time, history, impressions = line.strip().split('\t')
         impressions = impressions.split(' ')
         pos = []
@@ -57,7 +57,7 @@ def save_data(data, file_rank, mode):
 def generate_testdata(raw_data_path, mode):
     Path(f'./data/speedy_data/{mode}').mkdir(parents=True, exist_ok=True)
     with open('./data/speedy_data/{}/ProtoBuf_0.tsv'.format(mode), 'w', encoding='utf-8') as f:
-        for line in open(os.path.join(raw_data_path, 'MINDlarge_{}/behaviors.tsv'.format(mode)), 'r', encoding='utf-8'):
+        for line in open(os.path.join(raw_data_path, f'MIND-{args.mode}/{mode}/behaviors.tsv'), 'r', encoding='utf-8'):
             impid, uid, time, history, impressions = line.strip().split('\t')
 
             if mode == 'dev':
@@ -77,6 +77,7 @@ def generate_testdata(raw_data_path, mode):
             f.write(case + '\n')
 
 def get_arguments():
+    global args
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -88,6 +89,10 @@ def get_arguments():
     parser.add_argument(
         "--file_num_for_traindata",
         default=512,
+    )
+    parser.add_argument(
+        "--mode",
+        default="large",
         type=str,
     )
     args = parser.parse_args()
@@ -95,9 +100,9 @@ def get_arguments():
 
 def main():
     args = get_arguments()
-    assert os.path.exists(os.path.join(args.raw_data_path, 'MINDlarge_train'))
-    assert os.path.exists(os.path.join(args.raw_data_path, 'MINDlarge_dev'))
-    assert os.path.exists(os.path.join(args.raw_data_path, 'MINDlarge_test'))
+    assert os.path.exists(os.path.join(args.raw_data_path, f'MIND-{args.mode}/train'))
+    assert os.path.exists(os.path.join(args.raw_data_path, f'MIND-{args.mode}/dev'))
+    assert os.path.exists(os.path.join(args.raw_data_path, f'MIND-{args.mode}/test'))
 
     Path('./data/speedy_data/').mkdir(parents=True, exist_ok=True)
     generate_news(args.raw_data_path)
